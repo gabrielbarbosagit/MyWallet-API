1492aa6add178ccb4039c43a74248c4986e19b65
gabriel barbosa <gabrielb_lima@hotmail.com>
2023-07-09 19:49:54 -0300

    changes app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(json());
-app.use(router);
+
 
 app.listen(PORT, () =>  console.log(`Server listening on ${PORT}`));
