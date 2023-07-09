5da926e790c3c3578d23005b825b4fb4f87ae7c6
gabriel barbosa <gabrielb_lima@hotmail.com>
2023-07-09 20:56:04 -0300

    routes - test

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-
-
+import router from "../routes/index.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
+app.use(router);
 
-
-app.listen(PORT, () =>  console.log(`Server listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
